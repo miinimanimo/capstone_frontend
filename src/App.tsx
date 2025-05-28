bc039f318e04cd4ad4eeac3a2cbfe6fee6d5ed4e
miinimanimo <ssk07079@gmail.com>
2025-05-28 22:34:52 +0900

    화면 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/capstone_frontend">
       <div className="app">
         <Navbar />
         <div className="content">
