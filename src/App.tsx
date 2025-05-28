0150e88bd17278e4af4fdbc6de08d7165ad3db06
miinimanimo <ssk07079@gmail.com>
2025-05-28 22:43:38 +0900

    다시 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
